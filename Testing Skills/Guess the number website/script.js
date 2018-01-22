75ea279b5fea4dbd67a0a7b9bb148c81ac31553e
Jax <Jaxtubbs@gmail.com>
2018-01-21 16:17:36 -0800

    guess the number complete

---
+++ Testing Skills/Guess the number website/script.js
@@ -0,0 +1,26 @@
+var response = document.getElementById("response")
+var randNum = parseInt(Math.random() * 100 + 1);
+window.addEventListener("keypress", myEventHandler, false);
+console.log(randNum)
+
+    
+function myEventHandler(e) {
+
+    var keyCode = e.keyCode;
+
+    if (keyCode == 13) {
+        var input = document.getElementById("answer").value
+        if (input <= 100 && input >= 1) {
+            if (input > randNum) {
+                response.innerHTML = "Your guess is too high";
+            } else if (input < randNum){
+                response.innerHTML = "Your guess is too low";
+            }else{
+            	response.innerHTML = "You win!";
+            }
+        } else {
+            response.innerHTML = "You guess is not a valid answer.";
+        }
+        document.getElementById("answer").value = null;
+    }
+};
