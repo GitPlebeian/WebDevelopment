18f22f30883d1fd24e6e6d8f4ce758211a78b7cc
Jax <Jaxtubbs@gmail.com>
2018-01-31 23:39:16 -0800

    adfasdf

---
+++ Javascript/P5/FirstP5.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  background(51)
+  frameRate(60)
+}
+function draw() {
+  fill(255)
+  rect(mouseX,mouseY)
+}
---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(640, 480);
+  frameRate(500)
+}
+
+
+function draw() {
+  // put drawing code here
+  if (mouseIsPressed) {
+  fill(0);
+} else {
+  fill(255);
+}
+ellipse(mouseX, mouseY, 80, 80);
+}
