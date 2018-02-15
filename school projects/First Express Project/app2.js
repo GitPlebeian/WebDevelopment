ff5c12bfaf32028eb26be49ec3b681d6b1340962
Jackson Tubbs-150002955 <150002955@vusdnoc.vusd.k12.ca.us>
2018-02-15 13:03:32 -0800

    wrerrereer

--- school projects/First Express Project/app2.js
+++ school projects/First Express Project/app2.js
@@ -1,10 +1,8 @@
 var express = require('express')
 var app = express()
-
-console.log(__dirname);
-
-app.get('/',function(request,response){
-  response.sendFile(__dirname + "/views/index2.html")
+app.use(express.static(__dirname + "/"))
+app.get('/',function(req,res){
+  res.sendFile(__dirname + "/views/index2.html")
 })
 app.listen(3000, function(){
   console.log("Server Starting")
