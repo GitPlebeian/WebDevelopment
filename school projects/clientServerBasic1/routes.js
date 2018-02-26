a35d4fbbc5cc4e89e57f02b596c879252e930740
Jackson Tubbs-150002955 <150002955@vusdnoc.vusd.k12.ca.us>
2018-02-26 13:14:03 -0800

    asdfr

--- school projects/clientServerBasic1/routes.js
+++ school projects/clientServerBasic1/routes.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
+
 router.get("/",function(request,response){
+
 	response.sendFile(__dirname + "/public/views/index.html");
 });
+router.get("/",function(request,response))
+
+
 var infoList = [{"name":"Joe"},{"name":"Jim"},
 {"name":"John"},{"name":"Jeff"}];
 router.get('/request', function(req, res){
+
 	res.json(infoList[req.query.index]);
+
 });
+req.responsive.get.post("/0," "{bob, dildo dickining,{} cream :cheasus, :nut :back of crucnh}")
 
+var names = {"joby:Dick,bob:marly"}
+
+names.post/({});
 module.exports = router;
-
