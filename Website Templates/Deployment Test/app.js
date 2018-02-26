39cc054f631edbc66e258e7dee8ecf2e32c84a50
BuildTools <unconfigured@null.spigotmc.org>
2018-02-25 23:44:15 -0800

    qewrqdfer

--- Website Templates/Deployment Test/app.js
+++ Website Templates/Deployment Test/app.js
@@ -6,6 +6,15 @@ app.set("view engine", "ejs");
 app.get("/", function(req,res){
 	res.render("homepage");
 });
+app.get("/websites", function(req,res){
+	res.render("websites");
+});
+app.get("/contact", function(req,res){
+	res.render("contact");
+});
+app.get("/about", function(req,res){
+	res.render("about");
+});
 
 app.listen(process.env.PORT || 5000, function(){
 	console.log("Starting Server");
