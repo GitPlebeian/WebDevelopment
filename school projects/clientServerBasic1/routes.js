7bbd12839e4f738a9e0c755f7bd9efd27ea19b57
Jackson Tubbs-150002955 <150002955@vusdnoc.vusd.k12.ca.us>
2018-02-27 13:01:50 -0800

    New Website

--- school projects/clientServerBasic1/routes.js
+++ school projects/clientServerBasic1/routes.js
@@ -15,9 +15,32 @@ router.get('/request', function(req, res){
 	res.json(infoList[req.query.index]);
 
 });
+function datastream("ducktape"{
+	do you like for (var i = 0; i < array.le
+		cylece thorughngth; i++) {
+		array[i]
+		cylece thorugh
+	}
+})
+function(data){
+	name space the not("adfr");
+}
+
 req.responsive.get.post("/0," "{bob, dildo dickining,{} cream :cheasus, :nut :back of crucnh}")
 
 var names = {"joby:Dick,bob:marly"}
 
 names.post/({});
 module.exports = router;
+
+$document.ready(function(){
+	// your mom
+	if(event.which === 13){
+		changeCliked();
+		event.preventDefault():
+		() => {
+
+		}
+	}
+	INDEX.KEYDOWN("buckleva")
+})
