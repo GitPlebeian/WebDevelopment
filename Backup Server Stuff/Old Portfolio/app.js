9acf4a02e3c7810f2d5414cf121e1ea29a342403
BuildTools <unconfigured@null.spigotmc.org>
2018-03-06 23:49:20 -0800

    commited

---
+++ Backup Server Stuff/Old Portfolio/app.js
@@ -0,0 +1,37 @@
+express = require('express');
+app = express();
+
+var http = require('https')
+var fs = require('fs')
+
+var sslPath = '/etc/letsencrypt/live/www.jaxtubbs.site/'
+
+var options = {
+	key: fs.readFileSync(sslPath + 'privkey.pem'),
+	cert: fs.readFileSync(sslPath + 'fullchain.pem')
+}
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res){
+	console.log("Someone Connected")
+	res.render("homepage");
+});
+app.get("/websites", function(req,res){
+	res.render("websites");
+});
+app.get("/contact", function(req,res){
+	res.render("contact");
+});
+
+app.get("/single-page", function(req,res){
+	res.render("websites/onePage");
+});
+app.get("/parlax-photography", function(req,res){
+	res.render("websites/paralaxPhotography");
+});
+
+server = http.createServer(options, app)
+io = require('socket.io').listen(server)
+server.listen(443)
--- Website Templates/Deployment Test/app.js
+++ Website Templates/Deployment Test/app.js
@@ -1,15 +1,5 @@
-express = require('express');
+var express = require('express');
 app = express();
-
-var http = require('https')
-var fs = require('fs')
-
-var sslPath = '/etc/letsencrypt/live/www.jaxtubbs.site/'
-
-var options = {
-	key: fs.readFileSync(sslPath + 'privkey.pem'),
-	cert: fs.readFileSync(sslPath + 'fullchain.pem')
-}
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -19,23 +9,6 @@ app.get("/", function(req,res){
 	res.render("homepage");
 });
 
-app.get('/health-check',(req,res) => res.sendStatus(200));
-
-
-app.get("/websites", function(req,res){
-	res.render("websites");
+app.listen(5000, function(){
+	console.log("Starting Server");
 });
-app.get("/contact", function(req,res){
-	res.render("contact");
-});
-
-app.get("/single-page", function(req,res){
-	res.render("websites/onePage");
-});
-app.get("/parlax-photography", function(req,res){
-	res.render("websites/paralaxPhotography");
-});
-
-server = http.createServer(options, app)
-io = require('socket.io').listen(server)
-server.listen(443)
