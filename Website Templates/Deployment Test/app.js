86ce0ac611d6f1ca3ceb036be57e370f49ac6d4c
BuildTools <unconfigured@null.spigotmc.org>
2018-03-13 08:34:06 -0700

    commited

--- Website Templates/Deployment Test/app.js
+++ Website Templates/Deployment Test/app.js
@@ -8,15 +8,15 @@ app.get("/", function(req,res){
 	console.log("Someone Connected")
 	res.render("homepage");
 });
-
 app.get("/websites", function(req,res){
 	res.render("websites");
 });
-
 app.get("/websites/business", function(req,res){
-	res.render("business.ejs");
+	res.render("websites/business.ejs");
 });
-
+app.get("/websites/business/products", function(req,res){
+	res.render("websites/business1.ejs");
+});
 
 app.listen(5000, function(){
 	console.log("Starting Server");
