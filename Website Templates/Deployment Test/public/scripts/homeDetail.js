d9247df5955651b4fa74949cf2d1d13717f99a1a
BuildTools <unconfigured@null.spigotmc.org>
2018-04-10 06:33:09 -0700

    commited

--- Website Templates/Deployment Test/public/scripts/homeDetail.js
+++ Website Templates/Deployment Test/public/scripts/homeDetail.js
@@ -15,18 +15,43 @@ function navBar(){
 function accordianToggle(thing) {
   things = thing.nextElementSibling
   obj = $(thing.nextElementSibling);
+  $('.accordianToggle').removeClass('active')
+  $(thing).toggleClass('active')
   allAcorrdions = $('.accordianContent')
   allAcorrdions.each(function(){
     $(this).css('max-height','0')
   })
-  if(things.style.maxHeight >= 20){
+  if($(thing).hasClass('active')){
     obj.css('max-height','0')
     setTimeout(function(){
       obj.css('border-width','0px')
     },205)
+    console.log('meme');
   } else {
-    console.log('asdf');
     obj.css('border-width','1px')
     things.style.maxHeight = things.scrollHeight + 'px'
   }
+}
+
+function hasTouch() {
+    return 'ontouchstart' in document.documentElement
+           || navigator.maxTouchPoints > 0
+           || navigator.msMaxTouchPoints > 0;
+}
+
+if (hasTouch()) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
 }
