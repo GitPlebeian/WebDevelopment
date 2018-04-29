9671c81ab15c608e1b21f5f3e622c10a7a1ba082
BuildTools <unconfigured@null.spigotmc.org>
2018-04-28 18:20:39 -0700

    commited

--- Website Templates/Deployment Test/public/scripts/homepage.js
+++ Website Templates/Deployment Test/public/scripts/homepage.js
@@ -2,13 +2,24 @@ window.onbeforeunload = function () {
   window.scrollTo(0,0);
 }
 
+var active = true
 function navBar(){
   navBarToggle = $('.navBarToggle')
   obj = document.getElementById("navBarLinks")
 
   $(obj).css('color',"red")
   navBarToggle.toggleClass("active")
-  $('.navBar').toggleClass('active')
+
+  if(active){
+    $('.navBar').toggleClass('active')
+    active = !active
+  } else {
+    setTimeout(function() {
+      $('.navBar').toggleClass('active')
+    }, 150)
+    active = !active
+  }
+
   if(obj.style.maxHeight){
     obj.style.maxHeight = null
   } else {
